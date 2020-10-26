e5e18124175ae642bb66012ca0d0e8010e85eeec
Lanzhou Jiang <lzjiang99@gmail.com>
2020-10-26 23:35:20 +1100

    refactored test code, green bar

--- BowlingGameKataTest/BowlingGameTests.cs
+++ BowlingGameKataTest/BowlingGameTests.cs
@@ -16,27 +16,22 @@ namespace BowlingGameKataTest
         [Fact]
         public void TestGutterGame()
         {
-            int n = 20;
-            int pins = 0;
-            RollMany(n, pins);
+          
+            RollMany(20, 0);
             Assert.Equal(0, g.Score());
         }
 
         private void RollMany(int n, int pins)
         {
-            for (int i = 0; i < 20; i++)
-            {
+            for (int i = 0; i < n; i++)
                 g.Roll(pins);
-            }
+            
         }
 
         [Fact]
         public void TestAllOnes()
         {
-            for (int i = 0; i < 20; i++)
-            {
-                g.Roll(1);
-            }
+            RollMany(20,1);
             Assert.Equal(20, g.Score());
         }
     }
