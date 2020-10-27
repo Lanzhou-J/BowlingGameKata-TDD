54e4310cf7662b67cf2d3db35ea6dbbd10c278ce
Lanzhou Jiang <lzjiang99@gmail.com>
2020-10-27 13:45:52 +1100

    refactored production code, extract methods, the fourth test - green bar

--- BowlingGameKata/Game.cs
+++ BowlingGameKata/Game.cs
@@ -18,21 +18,36 @@ namespace BowlingGameKata
              {
                  if (IsStrike(frameIndex))
                  {
-                     score += 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+                     score += 10 + StrikeBonus(frameIndex);
                      frameIndex++;
                  }
                  else if (IsSpare(frameIndex))
                  {
-                     score += 10 + _rolls[frameIndex + 2];
+                     score += 10 + SpareBonus(frameIndex);
                      frameIndex += 2;
                  }
                  else
                  {
-                     score += _rolls[frameIndex] + _rolls[frameIndex+1];
+                     score += SumOfBallsInFrame(frameIndex);
                      frameIndex += 2;
                  }
              }
              return score;
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+        }
+
+        private int SumOfBallsInFrame(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex+1];
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return _rolls[frameIndex + 2];
         }
 
         private bool IsStrike(int frameIndex)
