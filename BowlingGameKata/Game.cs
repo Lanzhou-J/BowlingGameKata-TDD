f405f12cef888f51624940b7a453e47211e8774a
Lanzhou Jiang <lzjiang99@gmail.com>
2020-10-27 11:44:10 +1100

    refactored production code, changed design, the third test - red bar

--- BowlingGameKata/Game.cs
+++ BowlingGameKata/Game.cs
@@ -1,15 +1,23 @@
+using System.Collections.Generic;
+
 namespace BowlingGameKata
 {
     public class Game
     {
         private int _score = 0;
+        private readonly int[] _rolls = new int[21];
+        private int _currentRoll = 0;
         public void Roll(int pins)
         {
             _score += pins;
+            _rolls[_currentRoll++] = pins;
         }
         public int Score()
         {
-            return _score;
+             int score = 0;
+             for (int i = 0; i < _rolls.Length; i++)
+                 score += _rolls[i];
+             return score;
         }
     }
 }
