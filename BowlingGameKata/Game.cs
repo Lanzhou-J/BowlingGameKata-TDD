e513b40c3e07ed07f575eac69c555c050946bf30
Lanzhou Jiang <lzjiang99@gmail.com>
2020-10-27 12:05:45 +1100

    modified production code, Score function, add spare logic, the third test - green bar

--- BowlingGameKata/Game.cs
+++ BowlingGameKata/Game.cs
@@ -16,8 +16,16 @@ namespace BowlingGameKata
              int i = 0;
              for (int frame = 0; frame < 10; frame++)
              {
-                 score += _rolls[i] + _rolls[i+1];
-                 i += 2;
+                 if (_rolls[i] + _rolls[i+1] == 10)
+                 {
+                     score += 10 + _rolls[i + 2];
+                     i += 2;
+                 }
+                 else
+                 {
+                     score += _rolls[i] + _rolls[i+1];
+                     i += 2;
+                 }
              }
              return score;
         }
